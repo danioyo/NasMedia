49ac1da4bcd53edffec761ec0c2deb6426ae2c41
danioyo <danisufaru@gmail.com>
2026-08-02 14:56:13 +0300

    Add play button to hero section and style enhancements

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const navbar = document.querySelector(".navbar");
 const navElem = document.querySelectorAll(".navbar a");
 const bubble = document.querySelector('.bubble');
 
+const API_KEY = '8b1e43728b1c17cc47afd3fde208e6b4';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const HERO_IMAGE_URL = 'https://image.tmdb.org/t/p/w1280';
+
+
 function moveBubble(element){
     const width = element.offsetWidth + 62;
     const left = element.offsetLeft - (62 / 2);
